b042047678b96227aef48f5279cb0a1d0edd1c54
Derek Dongray <derek@valedon.co.uk>
2018-09-07 08:13:47 +0100

    Initial

---
+++ LocateFolder/LocateFolderCommand.cs
@@ -0,0 +1,123 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using LocateFolder.Utilities;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using Task = System.Threading.Tasks.Task;
+
+namespace LocateFolder
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class LocateFolderCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("f31449ab-b753-48fe-bf3f-25c691905367");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        private DTE2 _applicationObject { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocateFolderCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private LocateFolderCommand(AsyncPackage package, OleMenuCommandService commandService, DTE2 appObj)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            this._applicationObject = appObj ?? throw new ArgumentNullException(nameof(_applicationObject));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static LocateFolderCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in LocateFolderCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            DTE2 applicationObject = await package.GetServiceAsync(typeof(DTE)) as DTE2;
+            Instance = new LocateFolderCommand(package, commandService, applicationObject);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "LocateFolderCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+            UIHierarchy uih = _applicationObject.ToolWindows.SolutionExplorer;
+            var selectedItems = ((Array)uih.SelectedItems);
+
+            if (selectedItems != null)
+            {
+                LocateFile.FilesOrFolders((IEnumerable<string>)(from object t in selectedItems
+                                                                where (t as UIHierarchyItem)?.Object is ProjectItem
+                                                                select ((ProjectItem)((UIHierarchyItem)t).Object).FileNames[1]));
+            }
+        }
+    }
+}
